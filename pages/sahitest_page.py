1e8a4866a3ceb37a58ac9c7e09f751f674bb40f4
zhiheng <zhiheng.hu@klook.com>
2022-01-30 21:42:40 +0800

    feat: page object case

--- pages/sahitest_page.py
+++
@@ -1,8 +0,0 @@
-from poium import Page, Element
-
-
-class SahiPage(Page):
-    link_text = Element(link_text="Link Test", describe="Link Test")
-    input_text = Element(id_="checkRecord", describe="input Text")
-    input_button = Element(css="input[value='Click me']", describe="input button")
-
