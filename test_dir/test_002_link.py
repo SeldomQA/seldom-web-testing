a03868dfa9377f0dc5863c7af9b91bc92e0b9739
zhiheng <zhiheng.hu@klook.com>
2022-01-30 20:36:32 +0800

    feat: new case

---
+++ test_dir/test_002_link.py
@@ -0,0 +1,20 @@
+import seldom
+
+
+class LinkTest(seldom.TestCase):
+
+    def test_link(self):
+        """
+        测试链接
+        """
+        self.open("https://sahitest.com/demo/linkTest.htm")
+        self.click_text("linkByContent")
+        self.sleep(2)
+        self.click_text("Back")
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
+
+
+
---
+++ test_dir/test_003_iframe.py
@@ -0,0 +1,20 @@
+import seldom
+
+
+class IFrameTest(seldom.TestCase):
+
+    def test_iframe(self):
+        """
+        测试嵌套表单
+        """
+        self.open("https://sahitest.com/demo/iframesTest.htm")
+        self.switch_to_frame(xpath="//iframe")
+        title = self.get_text(xpath="//h2")
+        self.assertEqual(title, "Sahi Tests")
+        self.switch_to_frame_out()
+        title = self.get_text(xpath="//h2")
+        self.assertEqual(title, "IFRAME Tests")
+
+
+if __name__ == '__main__':
+    seldom.main(debug=True)
