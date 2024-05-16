7e353d148ba0dc25831d0a3f0ba46a6ca87482eb
defnngj <defnngj@gmail.com>
2024-05-16 16:07:01 +0800

    feat: run report.

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import seldom
 if __name__ == '__main__':
     seldom.main(
         path="./test_dir",
-        browser="gc",
-        debug=False  # debug 开关 True/False
+        browser="gc", # google chrome
+        rerun=3,
     )
