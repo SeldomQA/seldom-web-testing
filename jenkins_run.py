e814ce6e883e4963ed2bc1d943a8eed66b337684
defnngj <defnngj@gmail.com>
2025-04-26 18:26:45 +0800

    test: update run script

--- jenkins_run.py
+++ jenkins_run.py
@@ -1,20 +1,17 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-
-options = webdriver.EdgeOptions()
-options.add_argument('--disable-features=msEdgeSandbox')
-options.add_argument('--remote-debugging-port=9222')
-options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+import seldom
+from selenium.webdriver import EdgeOptions
 
 
-service = EdgeService(
-    executable_path=r'C:\Users\fnngj\.cache\selenium\msedgedriver\win64\136.0.3240.29\msedgedriver.exe',   # 需绝对路径
-    service_args=['--enable-chrome-logs']                 # 启用详细日志
-)
-
-driver = webdriver.Edge(options=options, service=service)
-driver.get("https://cn.bing.com")
-time.sleep(3)
-driver.quit()
-
+if __name__ == '__main__':
+    edge_option = EdgeOptions()
+    edge_option.add_argument("--headless=new")
+    browser = {
+        "browser": "edge",
+        "options": edge_option
+    }
+    seldom.main(
+        path="./test_dir",
+        browser=browser,  # edge
+        rerun=3,
+        open=False
+    )
--- run.py
+++ run.py
@@ -1,19 +1,10 @@
 import seldom
-from selenium.webdriver import EdgeOptions
 
 
 if __name__ == '__main__':
-    edge_option = EdgeOptions()
-    edge_option.add_argument("--headless=new")
-    edge_option.add_argument('--disable-features=msEdgeSandbox')
-    edge_option.add_argument('--remote-debugging-port=9222')
-    browser = {
-        "browser": "edge",
-        "options": edge_option
-    }
     seldom.main(
         path="./test_dir",
-        browser=browser,  # edge
+        browser="edge",  # edge
         rerun=3,
         open=False
     )
