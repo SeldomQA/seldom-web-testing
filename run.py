018386e780d8c417b76ea4238a1161749168edb1
defnngj <defnngj@gmail.com>
2025-04-26 17:02:03 +0800

    test: jenkins run

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from selenium.webdriver import EdgeOptions
 if __name__ == '__main__':
     edge_option = EdgeOptions()
     edge_option.add_argument("--headless=new")
-
+    edge_option.add_argument('--disable-features=msEdgeSandbox')
+    edge_option.add_argument('--remote-debugging-port=9222')
     browser = {
         "browser": "edge",
         "options": edge_option
