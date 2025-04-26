bb35f98f76fc3380cd5de703d09dd7bebc9072d0
defnngj <defnngj@gmail.com>
2025-04-26 17:28:34 +0800

    test: jenkins run

--- jenkins_run.py
+++ jenkins_run.py
@@ -1,10 +1,18 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.service import Service as EdgeService
 
 options = webdriver.EdgeOptions()
+options.add_argument('--disable-features=msEdgeSandbox')
+options.add_argument('--remote-debugging-port=9222')
 options.binary_location = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-service = Service(executable_path=r"C:\Users\fnngj\.cache\selenium\msedgedriver\win64\136.0.3240.29\msedgedriver.exe")
+
+
+service = EdgeService(
+    executable_path=r'C:\Users\fnngj\.cache\selenium\msedgedriver\win64\136.0.3240.29\msedgedriver.exe',   # 需绝对路径
+    service_args=['--enable-chrome-logs']                 # 启用详细日志
+)
+
 driver = webdriver.Edge(options=options, service=service)
 driver.get("https://cn.bing.com")
 time.sleep(3)
