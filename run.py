a44bd674389bdd2bf557f01a70b2c2bc166a49cd
defnngj <defnngj@gmail.com>
2025-04-26 15:39:01 +0800

    test: jenkins run

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 import seldom
+from selenium.webdriver import EdgeOptions
 
 
 if __name__ == '__main__':
+    edge_option = EdgeOptions()
+    edge_option.add_argument("--headless=new")
+
+    browser = {
+        "browser": "edge",
+        "options": edge_option
+    }
     seldom.main(
         path="./test_dir",
-        browser="edge", # edge
+        browser=browser,  # edge
         rerun=3,
+        open=False
     )
